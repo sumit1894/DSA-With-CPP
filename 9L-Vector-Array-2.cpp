191411bf64495d7b38e380305247becba8a2b7f0
Sumit Prasad <sumit.study1894@gmail.com>
2024-09-14 00:37:06 +0530

    Vector

--- 9L-Vector-Array-2.cpp
+++ 9L-Vector-Array-2.cpp
@@ -2,6 +2,25 @@
 // #include<vector>
 using namespace std;
 
+// line 152
+void reverseVector(vector<int> &vec){
+
+    int start=0;
+    int end=vec.size()-1;
+    
+    while(start<end){
+        swap(vec[start],vec[end]);
+        start++;
+        end--;
+    }
+cout<<"in Function:-"<<endl;
+for(int value: vec){
+    cout<<value<<" ";
+}
+cout<<endl;
+}
+
+
 int main(){
 
     cout<<"----------------------Vector----------------------7"<<endl<<endl;
@@ -32,7 +51,7 @@ int main(){
         cout<<value<<" ";
     }
 
-    cout<<endl;
+    cout<<endl<<endl;
 
     cout<<"---------------------- Vector-Function---------------------37"<<endl<<endl;
 
@@ -106,17 +125,54 @@ int main(){
     vec12.push_back(1); // 1 size and 1 capacity
     vec12.push_back(2); // 2 size and 2 capacity
     vec12.push_back(3);// 3 size and 4 capacity
+    vec12.push_back(4);// 4 size and 4 capacity
+    vec12.push_back(5);// 4 size and 8 capacity
 
     cout<<"Vector size is "<<vec12.size()<<endl;
     cout<<"Vector capacity is "<<vec12.capacity()<<endl;
+
+    cout<<endl<<endl;
+
+    cout<<"---------------------- Single Number---------------------117"<<endl<<endl;
+
+    cout<<"Find the single number/ unique number in an array"<<endl;
+
+    vector<int> vec13={1,2,3,4,1,2,3};
+    int sum=0;
+    for(int value: vec13){
+        sum=sum^value;
+    }
+    cout<<"Single Number is "<<sum<<endl;
     
+    cout<<endl<<endl;
     
+    cout<<"----------------------------------- HomeWork---------------------------------130"<<endl<<endl;
     
+    cout<<"Linear Search in Vector"<<endl;
+
+    vector<int> vec14={4,2,7,8,1,2,5};
+    int target=8;
+    for(int value: vec14){
+        if(value==target){
+            cout<<"Element Found:- "<<target<<endl;
+            break;
+        }
+    }
+
+    cout<<endl<<endl;
     
+    cout<<"Reverse an Vector using Function"<<endl;
+
+    vector<int> vec15={1,2,3,4,5};
+    reverseVector(vec15);
+    cout<<endl;
+    cout<<"in Main:-"<<endl;
+    for(int value: vec15){
+        cout<<value<<" ";
+    }
+
     
-    
-    
-    
+
     return 0;
 
 
