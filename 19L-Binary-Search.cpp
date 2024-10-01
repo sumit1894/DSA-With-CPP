a766783f2c401cd14984b5dc2ee4196a31afec01
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-02 00:09:54 +0530

    Peak Index in Mountain Array

--- 19L-Binary-Search.cpp
+++ 19L-Binary-Search.cpp
@@ -1,39 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-    
-void peakIndex(int arr[],int n){
-
+int PeakIndexMountainArray(vector<int>& arr, int n)
+{
     // peakindex cannot be a start value and end value
-    int st=1;
-    int end=n-2;
+    int st = 1;
+    int end = n - 2;
     int mid;
 
-
-    while (st<=end)
-     mid=st+(end-st)/2;
-    {
-        if(arr[mid-1]<arr[mid]>arr[mid+1]){
-            cout<<mid;
+    while (st <= end) {
+        mid = st + (end - st) / 2;
+        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+        {
+            return mid;
         }
-        if(arr[mid-1]<arr[mid]){
-            st=mid+1;
+        else if (arr[mid - 1] < arr[mid])
+        {
+            st = mid + 1;
         }
-        else{
-            
-            end=mid-1;
+        else
+        {
+            end = mid - 1;
         }
     }
+    return -1;
 }
- 
 
 int main()
 {
 
-    int arr[]= {0, 3, 8, 9, 5, 2};
-    int n = 6;
-
-    peakIndex(arr,n);
+    vector<int> arr = {0, 3, 8, 9, 5, 2};
+    int n = arr.size();
+    int result=PeakIndexMountainArray(arr,n);
+    cout<<result;
 
     return 0;
 }
