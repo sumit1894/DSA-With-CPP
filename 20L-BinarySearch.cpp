ac77a2ccf10c7863a681a3009859201517f444ac
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-03 23:35:07 +0530

    Single Element in Sorted Array

--- 20L-BinarySearch.cpp
+++ 20L-BinarySearch.cpp
@@ -12,6 +12,45 @@ int LinearSearchSingleElement(vector<int> &arr){
 }
 
 
+int SingleElement(vector<int> &arr){
+    int n=arr.size();
+    int st=1;
+    int end=n-1;
+    if(n==1) return arr[0];
+
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        if(mid==0 && arr[0]!=arr[1]){
+            return arr[mid];
+        }
+        if(mid==(n-1) && arr[n-1]!=arr[n-2]){
+            return arr[mid];
+        }
+
+        if(arr[mid-1] != arr[mid] && arr[mid]!=arr[mid+1]){
+            return arr[mid];
+        }
+
+        if(mid%2==0){//even
+            if(arr[mid]==arr[mid+1]){//right
+                st=mid+2;
+            }
+            else{//left
+                end=mid-1;
+            }
+        }
+        else{//odd
+            if(arr[mid]==arr[mid-1]){//right
+                st=mid+1;
+            }
+            else{//left
+                end=mid-1;
+            }
+        }
+    }
+    return -1;
+
+}
 
 
 int main(){
@@ -19,7 +58,6 @@ int main(){
 cout<<"--------------Single Element in Sorted Array--------------"<<endl<<endl;
 
 vector<int> arr={1,1,2,3,3,4,4,8,8};
-// vector<int> arr={3,3,7,7,10,11,11};
 int ans=LinearSearchSingleElement(arr);
 cout<<"Single Element in the array is: "<<ans<<endl;
 
@@ -27,8 +65,9 @@ cout<<"Single Element in the array is: "<<ans<<endl;
 cout<<endl<<"----------------------------------------------------------"<<endl;
 
 
-// int ans2=SingleElement(arr);
-// cout<<"Single Element in the array is: "<<ans2<<endl;
+vector<int> arr1={3,3,7,7,10,11,11};
+int ans2=SingleElement(arr1);
+cout<<"Single Element in the array is: "<<ans2<<endl;
 
 
     return 0;
